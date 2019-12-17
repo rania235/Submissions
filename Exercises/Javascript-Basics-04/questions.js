311d50e22cc195d81b77a05f0ba79970b098c06d
rania235 <rania.obeid1996@gmail.com>
2019-12-17 10:44:22 +0200

    javascript-basics-04

--- Exercises/Javascript-Basics-04/questions.js
+++ Exercises/Javascript-Basics-04/questions.js
@@ -1,101 +1,101 @@
 
 const stringSize = (text) => {
-
+    return text.length();
 }
 const replaceCharacterE = (text) => {
-
+    return text.replaceCharacterE();
 }
 const concatString = (text1, text2) => {
-
+    return text.length();
 }
 const showChar5 = (text) => {
-
+    return text.showChar5();
 }
 const showChar9 = (text) => {
-
+    return text.showChar9();
 }
 const toCapitals = (text) => {
-
+    return text.toCapitals();
 }
 const toLowerCase = (text) => {
-
+    return text.toLowerCase();
 }
 const removeSpaces = (text) => {
-
+    return text.removeSpaces();
 }
 const IsString = (text) => {
-
+    return text.IsString();
 }
 
 const getExtension = (text) => {
-
+    return text.getExtension();
 }
 const countSpaces = (text) => {
-
+    return text.countSpaces();
 }
 const InverseString = (text) => {
-
+    return text.InverseString();
 }
 
 const power = (x, y) => {
-
+    return text.power(x,y);
 }
 const absoluteValue = (num) => {
-
+    return text.absoluteValue(num);
 }
 const absoluteValueArray = (array) => {
-
+    return text.absoluteValueArray(array);
 }
 const circleSurface = (radius) => {
-
+    return text.circleSurface(radius);
 }
 const hypothenuse = (ab, ac) => {
-
+    return text.hypothenuse(ab, ac);
 }
 const BMI = (weight, height) => {
-
+    return text.BMI(weught , height);
 }
 
 const createLanguagesArray = () => {
-
+    return text.createLanguagesArray();
 }
 
 const createNumbersArray = () => {
-
+    return text.createNumbersArray();
 }
 
 const replaceElement = (languages) => {
-
+    return text.replaceElement(languagesl);
 }
 
 const addElement = (languages) => {
-
+    return text.addElement(languages);
 }
 
 const addNumberElement = (numbers) => {
-
+    return text.addNumberElement(numbers);
 }
 
 const removeFirst = (languages) => {
-
+    return text.removeFirst(languages);
 }
 
 const removeLast = (languages) => {
-
+    return text.removeLast(languages)
 }
 
 const convertStrToArr = (social_arr) => {
-
+    return text.convertStrToArr(social_arr);
 }
 
 const convertArrToStr = (languages) => {
-
+    return text.convertArrToStr(languages);
 }
 
 const sortArr = (social_arr) => {
-
+    return text.sortArr(social_arr);
 }
 
 const invertArr = (social_arr) => {
-
+    return text.invertArr(social_arr);
 }
