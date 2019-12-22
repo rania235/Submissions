0dfe373360122e91a3bd51eb74063419d303b048
rania235 <rania.obeid1996@gmail.com>
2019-12-22 13:18:10 +0200

    javascript-basics-05 ex 2 done

---
+++ Exercises/Javascript-Basics-04/javascript2.js
@@ -0,0 +1,11 @@
+<html>
+<script>
+var name = prompt("your name please ", "name");
+if (name == "Alice" || name == "Bob"){
+    alert("Hello " +name);
+}
+else{
+    alert("Hello Stranger ");
+}
+</script>
+</html>
