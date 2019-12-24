1c70ebf8462ba557ee7bf250fc81da5a344d3814
rania235 <rania.obeid1996@gmail.com>
2019-12-24 12:13:47 +0200

    javascript2.js ex 2 done

---
+++ Exercises/Javascript-Basics-04/javascript3.js
@@ -0,0 +1,16 @@
+<html>
+<script>
+var  x = prompt("enter your number");
+alert("hello " + x);
+
+
+
+</script>
+</html>
+
+
+
+
+Write a program that asks the user for a number n and prints the sum of the numbers 1 to n. Commit "loop" 🔑🔑
+ Example input: 5
+ Example output: 15
