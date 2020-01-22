39e6ca53d7641144c6b2d7a45d90a31a2576dc69
rania235 <rania.obeid1996@gmail.com>
2020-01-22 13:30:37 +0200

    part 3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -4,4 +4,19 @@ const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
+app.get("/test", (req, res) => {
+  res.send({
+    status: 200,
+    message: "ok"
+  });
+});
+
+app.get("/time", (req, res) => {
+  let today = new Date();
+  res.send({
+    status: 200,
+    message: today.getHours() + ":" + today.getMinutes()
+  });
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
